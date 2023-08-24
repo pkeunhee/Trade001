12eab312cdc89d2247aa68fef57390c7955ec808
박근희 <geunhui.park@navercorp.com>
2023-08-24 23:41:46 +0900

    백트레이더를 이용한 백테스트 샘플

---
+++ db/Backtrader_RSI.py
@@ -0,0 +1,37 @@
+import backtrader as bt
+import dbconn
+import backtrader as bt
+
+import dbconn
+
+class MyStrategy(bt.Strategy):  # ①
+    def __init__(self):
+        self.rsi = bt.indicators.RSI(self.data.close)  # ②
+    def next(self):  # ③
+        if not self.position:
+            if self.rsi < 30:
+                self.order = self.buy()
+        else:
+            if self.rsi > 70:
+                self.order = self.sell()
+
+code = '005930'
+start_date = '2022-01-03'
+end_date = '2023-08-18'
+
+mk = dbconn.MarketDB()
+corpDf = mk.get_daily_stock_price(code, start_date, end_date)
+corpDf2 = corpDf.drop(['code', 'date', 'name'], axis=1)
+corpDf2.columns = ['close', 'open', 'high', 'low', 'volume']
+
+cerebro = bt.Cerebro()  # ④
+cerebro.addstrategy(MyStrategy)
+data = bt.feeds.PandasData(dataname=corpDf2)
+cerebro.adddata(data)
+cerebro.broker.setcash(10000000)  # ⑥
+cerebro.addsizer(bt.sizers.SizerFix, stake=30)  # ⑦
+
+print(f'Initial Portfolio Value : {cerebro.broker.getvalue():,.0f} KRW')
+cerebro.run()  # ⑧
+print(f'Final Portfolio Value   : {cerebro.broker.getvalue():,.0f} KRW')
+cerebro.plot()  # ⑨
