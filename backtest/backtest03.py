9fd126fde09d908af8e9dc89120824cb60ea6a59
박근희 <geunhui.park@navercorp.com>
2023-09-08 13:45:07 +0900

    이평선을 이용한 예제

--- backtest/backtest03.py
+++ backtest/backtest03.py
@@ -1,6 +1,9 @@
 import backtrader as bt
 import backtrader.indicators as btind
 from datetime import date, datetime, time, timedelta
+
+import pandas as pd
+
 from db import dbconn
 
 
@@ -8,13 +11,23 @@ class MyStrategy(bt.Strategy):
     params = dict(
         fast_ma_period=10,
         slow_ma_period=30,
+        sma1_period=112,
+        sma2_period=224,
+        sma3_period=448,
+        sma4_period=5,
         stop_loss=0.03,
-        take_profit=0.07
+        take_profit=0.05
     )
 
     def __init__(self):
-        self.fast_ma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.fast_ma_period)
+        # self.fast_ma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.fast_ma_period)
         self.slow_ma = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.slow_ma_period)
+        self.sma1 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.sma1_period)
+        self.sma2 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.sma2_period)
+        # self.sma3 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.sma3_period)
+        self.sma4 = bt.indicators.SimpleMovingAverage(self.data.close, period=self.params.sma4_period)
+
+        self.crossover = bt.indicators.CrossOver(self.sma4, self.sma1)
 
         self.order = None
         self.stop_price = None
@@ -60,27 +73,51 @@ class MyStrategy(bt.Strategy):
         self.log('OPERATION PROFIT, GROSS %.2f, net %.2f' % (trade.pnl, trade.pnlcomm))
 
     def next(self):
+        # 매도
         if self.position:
             if self.data.close[0] >= self.take_price or \
                     self.data.close[0] <= self.stop_price:
                 self.sell()
             return
+        # 매수
         else:
-            if self.fast_ma[0] < self.slow_ma[0] and self.fast_ma[-1] > self.slow_ma[-1]:
+            # if self.fast_ma[0] < self.slow_ma[0] and self.fast_ma[-1] > self.slow_ma[-1]:
+            # 역배열 상황인지 확인
+            rev_array = self.sma2[0] > self.sma1[0] and self.sma1[0] > self.slow_ma[0]
+            if self.crossover > 0 and rev_array:
                 self.order = self.buy()
                 self.stop_price = self.data.close[0] * (1.0 - self.params.stop_loss)
                 self.take_price = self.data.close[0] * (1.0 + self.params.take_profit)
 
 
 if __name__ == '__main__':
-    start_date = '2022-01-02'
-    end_date = '2023-08-04'
-    codeList = ['005930', '000660', '035420', '035720', '207940']
-    # codeList = ['005930']
+    start_date = '2020-01-02'
+    end_date = '2023-08-31'
+    test = True
+    corpList = None
 
     mk = dbconn.MarketDB()
+    if test:
+        corpList = [
+            ['005930', '삼성전자']
+        ]
 
-    for code in codeList:
+        # corpList = [
+        #     ['005930', '삼성전자'],
+        #     ['000660', 'SK하이닉스'],
+        #     ['035420', 'NAVER'],
+        #     ['035720', '카카오'],
+        #     ['207940', '삼성바이오로직스']
+        # ]
+
+        corpList = pd.DataFrame(corpList, columns=['code', 'name'])
+    else:
+        corpList = mk.get_all_stock_corp()
+
+    for idx, row in corpList.iterrows():
+        code = row['code']
+        name = row['name']
+
         df = mk.get_daily_stock_price(code, start_date, end_date, 'S')
         df = df.drop(['code', 'date', 'name'], axis=1)
         df.columns = ['close', 'open', 'high', 'low', 'volume']
@@ -88,10 +125,10 @@ if __name__ == '__main__':
         data = bt.feeds.PandasData(dataname=df)
 
         cerebro = bt.Cerebro()
-        cerebro.broker.setcash(1000000)
-        # cerebro.broker.setcommission(commission=0.0014)  # ④
-        # cerebro.addsizer(bt.sizers.PercentSizer, percents=95)
-        cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+        cerebro.broker.setcash(10000000)
+        cerebro.broker.setcommission(commission=0.0014)  # ④
+        cerebro.addsizer(bt.sizers.PercentSizer, percents=95)
+        # cerebro.addsizer(bt.sizers.FixedSize, stake=10)
         cerebro.adddata(data)
         cerebro.addstrategy(MyStrategy)
 
@@ -104,6 +141,7 @@ if __name__ == '__main__':
         print(f'Final Portfolio Value   : {cerebro.broker.getvalue():,.0f} KRW')
 
         profit = (endAmt - startAmt) / startAmt * 100
-        print(f'수익률 : {profit}% KRW')
+        print(f'수익률 : {name} / {code}: {profit}% KRW')
+        print('--------------------------------------------------------------------------------------------------------------')
 
-        # cerebro.plot(style='candlestick')
+        cerebro.plot(style='candlestick')
